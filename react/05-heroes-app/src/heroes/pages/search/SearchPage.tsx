8b1a211309ec5a52eef9d0c38a46a3e260ff4889
Francisco Jose <franhr1113@gmail.com>
2025-10-30 18:47:28 +0100

    full despliege

--- react/05-heroes-app/src/heroes/pages/search/SearchPage.tsx
+++ react/05-heroes-app/src/heroes/pages/search/SearchPage.tsx
@@ -2,21 +2,39 @@ import { CustomJumbotron } from "@/components/custom/CustomJumbotron"
 import { HeroStats } from "@/heroes/components/HeroStats"
 import { SearchControl } from "./ui/SearchControl"
 import { CustomBreadcrumbs } from "@/components/custom/CustomBreadcrumbs"
+import { HeroGrid } from "@/heroes/components/HeroGrid"
+import { useQuery } from "@tanstack/react-query"
+import { useSearchParams } from "react-router"
+import { searchHeroesAction } from "@/heroes/actions/search-hero.action"
 
 
 export const SearchPage = () => {
+
+  const [searchParams] = useSearchParams()
+
+  const name = searchParams.get('name') ?? undefined
+  const fuerza = searchParams.get('fuerza') ?? undefined
+
+  const { data: heroes = [] } = useQuery({
+    queryKey: ['search', { name, fuerza }],
+    queryFn: () => searchHeroesAction({ name, fuerza }),
+    staleTime: 1000 * 60 * 5
+  })
+
+
+
   return (
     <>
-          <CustomBreadcrumbs currentPage="Buscar"
-          // breadcrumbs={
-          //   [
-          //     {label: 'Home', to: '/'},
-          //     {label: 'Home 1', to: '/'},
-          //     {label: 'Home 2', to: '/'},
-          //   ]
-          // }
-          
-          />
+      <CustomBreadcrumbs currentPage="Buscar"
+      // breadcrumbs={
+      //   [
+      //     {label: 'Home', to: '/'},
+      //     {label: 'Home 1', to: '/'},
+      //     {label: 'Home 2', to: '/'},
+      //   ]
+      // }
+
+      />
       {/* Header */}
       <CustomJumbotron
         title='Busqueda de Superheroes'
@@ -28,7 +46,11 @@ export const SearchPage = () => {
       {/* Stats Dashboard */}
       <HeroStats />
       {/* Filter y Busquedas */}
-      <SearchControl/>
+      <SearchControl />
+
+
+      <HeroGrid heroes={heroes} />
+
     </>
   )
 }
