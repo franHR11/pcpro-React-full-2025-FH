9a7e7b56ae5aeecaa56bd9461f320d03cc79c62a
Francisco Jose <franhr1113@gmail.com>
2025-10-31 15:44:22 +0100

    full despliege

--- react/05-heroes-app/src/heroes/pages/search/SearchPage.tsx
+++ react/05-heroes-app/src/heroes/pages/search/SearchPage.tsx
@@ -13,11 +13,11 @@ export const SearchPage = () => {
   const [searchParams] = useSearchParams()
 
   const name = searchParams.get('name') ?? undefined
-  const fuerza = searchParams.get('fuerza') ?? undefined
+  const fuerza = searchParams.get('strength') ?? undefined
 
   const { data: heroes = [] } = useQuery({
     queryKey: ['search', { name, fuerza }],
-    queryFn: () => searchHeroesAction({ name, fuerza }),
+    queryFn: () => searchHeroesAction({ name, strength }),
     staleTime: 1000 * 60 * 5
   })
 
